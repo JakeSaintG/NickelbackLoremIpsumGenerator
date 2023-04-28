02c9bb37abf7740a164f78b753196eaada36012c
Jake Saint Germain <jakesaint27@gmail.com>
2023-04-27 21:45:53 -0400

    commit WIP

--- Program.cs
+++ Program.cs
@@ -47,47 +47,7 @@ class Program
                 for (int i = 0; i < parsedInput; i++)
                 {
                     string addText = OutputHandler.GenerateLine(songList);
-
-                    //Adds a space between each line added except the first one.
-                    if (i == 0 || outputText.EndsWith(". "))
-                    {
-                        outputText += addText;
-                    }
-                    else
-                    {
-                        outputText += " " + addText;
-                    }
-
-                    //Some songs have an "I" as a pause that then goes into the next line. 
-                    //This statement simulates that in the Lorem Ipsum.
-                    if (outputText.EndsWith("I"))
-                    {
-                        outputText += "...";
-                    }
-
-                    //Some of the lines end in special characters.
-                    //Adds a period to every second line added to the text block. 
-                    //Prevents it from adding a period to a weird character.
-                    if (outputText.EndsWith("?") || outputText.EndsWith(")") || outputText.EndsWith(","))
-                    {
-                        //Do nothing. Don't let a period be placed after those specific characters.
-                    }
-                    else if (i % 2 == 0 && i != 0)
-                    {
-                        outputText += ". ";
-                    }
-
-                    //This statement handles adding a period to the end of the entire text block.
-                    //Works to prevent adding a period to a weird character (Note to self: Replace with regex later.)
-                    if (outputText.EndsWith(","))
-                    {
-                        outputText = outputText.Remove(outputText.Length - 1, 1) + ".";
-                    }
-                    else if (i == parsedInput - 1 && !outputText.EndsWith(" ") && !outputText.EndsWith("?"))
-                    {
-                        outputText += ".";
-                    }
-
+                    outputText += OutputHandler.FormatOutput(addText);
                 }
                 Console.WriteLine(outputText);
             }
